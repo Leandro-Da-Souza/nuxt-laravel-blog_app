2843ca7d7e515c0b01a5394e05f29bb68ae5a232
Leandro Da Souza <dasouza.leandro@gmail.com>
2023-03-03 17:48:04 +0100

    init commit, nuxt crash course laravel backend

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Post::factory(20)->create();
+        User::find(1)->update([
+            'name' => 'Leo',
+            'email' => 'leo@leo.com'
+        ]);
+    }
+}
